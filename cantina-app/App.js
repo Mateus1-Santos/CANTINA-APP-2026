c21c24a282cd8d32091bc3eab1839f27bb8d37a7
welli363 <toncosta08@gmail.com>
2026-02-12 11:26:56 -0300

    Update App.js

--- cantina-app/App.js
+++ cantina-app/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import cardapio from './assets/screens/Cardapio';
+import {useNavigation, NavigationContainer} from "@react-navigation/native"
+import Cardapio from './assets/screens/Cardapio';
+import LoginScreen from './screens/LoginScreens'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginScreens></LoginScreens>
-      <StatusBar style="auto" />
-    </View>
 
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Screen name='Cadastro' component={Cardapio}/>
+      <Stack.Screen name='Login' component-={LoginScreen}/>
+    </NavigationContainer>
   );
 }
 
