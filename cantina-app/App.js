a24cc2657270d618bc435c1e970f2c7d2585324e
DIEGO DE NEGREIROS SOARES <diegodenegreirossoares@gmail.com>
2026-02-19 08:33:10 -0300

    cadastro jr

--- cantina-app/App.js
+++ cantina-app/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import cardapio from './assets/screens/Cardapio';
-import LoginScreens from './screens/LoginScreens'
+import LoginScreens from './screens/LoginScreens';
+import Cadastro from './screens/cadastro';
 export default function App() {
   return (
 
 <LoginScreens></LoginScreens>
+
+<Cadastro></Cadastro>
   );
 }
 
